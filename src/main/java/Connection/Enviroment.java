8c3ca6daaa53cd2ad6df230789c0aa1cb7ab5421
roque92 <leroque.asrc@gmail.com>
2024-05-26 17:40:49 -0600

    Update Enviroment

--- src/main/java/Connection/Enviroment.java
+++ src/main/java/Connection/Enviroment.java
@@ -3,7 +3,7 @@ package Connection;
 public class Enviroment {
 
     public static final String URI = "https://v6.exchangerate-api.com/v6/";
-    public static final String API_KEY = ${{secrets.API_KEY}};
+    public static final String API_KEY = {{secrets.API_KEY}};
     public static final String URI_COMP= "/pair/";
     public static final String BASE_URL = URI+API_KEY+URI_COMP;
 }
